838870b67255d167430d55c722d37436fd26c027
Jaipaul <jaipaul076@gmail.com>
2022-02-18 22:38:26 +0530

    initial setup

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React ,{Component} from 'react';
+import { CardList } from './components/card-list/card-list.components';
+import './App.css';
+import {SearchBox} from  './components/search-box/search-box.components'
+
+class App extends Component{
+  constructor(){
+    super();
+    this.state={
+      monsters:[],
+      searchField:''
+    };
+  };
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response=> response.json())
+    .then(users=> this.setState({monsters:users}))
+  }
+  render(){
+    const {monsters,searchField} = this.state;
+    const filterdMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase()))
+    return (
+      <div className="App">
+      <h1> Monsters Rolodex </h1>
+      <SearchBox 
+      placeholder='search-box' 
+      handelChange={e=>this.setState({searchField:e.target.value})}
+      />
+      <CardList monsters={filterdMonsters}>
+      </CardList>
+       
+      </div>
+    );
+  }
+}
+
+export default App;
